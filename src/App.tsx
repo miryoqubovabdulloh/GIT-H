d4fa4ae21e24e7583b100828b850e99935efe1c4
Abdulloh <miryoqubovabdulloh628@gmail.com>
2025-10-23 18:40:50 +0500

    Ozgardi va css appla

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card"
+import Home from "./components/Home"
+
+function App() {
+  return (
+    <div>
+      <Card />
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+
+function Card() {
+  return (
+    <div>
+      <h1>hi</h1>
+      <p>Nima gap </p>
+    </div>
+  )
+}
+
+export default Card
