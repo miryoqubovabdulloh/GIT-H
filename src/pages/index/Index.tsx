ce154503c7e4d4f220219250927ae9bb9782b06b
Abdulloh <miryoqubovabdulloh628@gmail.com>
2025-10-27 12:50:03 +0500

    ozgardi

--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom"
 
 function Index() {
     return (
-        <div className="px-16 py-5">
-            <Link to={"/register"}>Registor page</Link>
+        <div className="flex items-center justify-center h-screen bg-gray-900">
+            <Link className="relative text-white text-2xl font-semibold tracking-wide
+                   after:content-[''] after:absolute after:left-0 after:bottom-0 
+                   after:w-0 after:h-[2px] after:bg-blue-500 
+                   hover:after:w-full after:transition-all after:duration-300
+                   hover:text-blue-600 transition-colors duration-300" to={"/register"}>Registor page</Link>
         </div>
     )
 }
