39f25c9f93c73b2802c4d1b5682d93c147c8fe6b
Abdulloh <miryoqubovabdulloh628@gmail.com>
2025-10-27 15:28:44 +0500

    jisdf

---
+++ src/components/Registor.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Registor() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <Link
+        to="/index"
+        className="relative text-white text-2xl font-semibold tracking-wide
+                   after:content-[''] after:absolute after:left-0 after:bottom-0 
+                   after:w-0 after:h-[2px] after:bg-blue-500 
+                   hover:after:w-full after:transition-all after:duration-300
+                   hover:text-blue-600 transition-colors duration-300"
+      >
+        Index page otish     
+      </Link>
+    </div>
+  );
+}
+
+export default Registor;
--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -7,4 +7,4 @@ function Index() {
     )
 }
 
-export default Index
+export default Index 
