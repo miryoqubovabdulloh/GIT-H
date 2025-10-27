54c14ee9173e1c7cc1e0200808ba39b19d731b4b
Abdulloh <miryoqubovabdulloh628@gmail.com>
2025-10-27 14:46:34 +0500

    ey bola

--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -7,9 +7,10 @@ function Index() {
                    after:content-[''] after:absolute after:left-0 after:bottom-0 
                    after:w-0 after:h-[2px] after:bg-blue-500 
                    hover:after:w-full after:transition-all after:duration-300
-                   hover:text-blue-600 transition-colors duration-300" to={"/register"}>Registor page</Link>
+                   hover:text-blue-600 transition-colors duration-300" to={"register"}>Registor page</Link>
         </div>
     )
 }
 
 export default Index
+ 
